e44603eeb8a7414377bc8a081a17032f81169349
Sergey <pmswga@gmail.com>
2018-07-22 20:28:25 +0300

    Удаление всех задач

--- Tasks/Extended tasker/1_4_1/main.cpp
+++
@@ -1,9 +0,0 @@
-///Написать программу, которая выводит на экран ваше имя и фамилию.
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    setlocale(LC_ALL, "RUS");
-    cout << "Басыров Сергей Амирович";
-}
